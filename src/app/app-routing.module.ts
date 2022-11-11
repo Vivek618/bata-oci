999f0458d1a45127e33efd271d215bbe4c579258
vivek618 <vivekanand618@gmail.com>
2022-11-11 16:09:14 +0530

    HTML Page Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormDetailComponent } from './form-detail/form-detail.component';
+import { TableDetailComponent } from './table-detail/table-detail.component';
 import { HomeComponent } from './home/home.component';
 // import { NevBarComponent } from './nev-bar/nev-bar.component';
 
@@ -11,7 +12,12 @@ const routes: Routes = [
   pathMatch:"full"
 },
 {
-  path:"HomeComponent",
+  path:"TableDetailComponent",
+  component:TableDetailComponent,
+  pathMatch:"full"
+},
+{
+  path:"",
   component:HomeComponent,
   pathMatch:"full"
 }
